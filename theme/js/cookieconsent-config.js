d408a1ebbcde28699f064d91dc4a23fe99a5d788
menteora <menteora@users.noreply.github.com>
2025-05-20 17:36:01 +0000

    deploy: 7d27bb2fdce73678ef93d4779ff4b436eb9abf0b

---
+++ theme/js/cookieconsent-config.js
@@ -0,0 +1,71 @@
+import 'https://cdn.jsdelivr.net/gh/orestbida/cookieconsent@3.1.0/dist/cookieconsent.umd.js';
+
+// Enable dark mode
+document.documentElement.classList.add('cc--darkmode');
+
+CookieConsent.run({
+    guiOptions: {
+        consentModal: {
+            layout: "box",
+            position: "bottom left",
+            equalWeightButtons: true,
+            flipButtons: false
+        },
+        preferencesModal: {
+            layout: "box",
+            position: "right",
+            equalWeightButtons: true,
+            flipButtons: false
+        }
+    },
+    categories: {
+        necessary: {
+            readOnly: true
+        },
+        analytics: {}
+    },
+    language: {
+        default: "it",
+        autoDetect: "browser",
+        translations: {
+            it: {
+                consentModal: {
+                    title: "Ciao viaggiatore, è tempo di biscotti!",
+                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.",
+                    acceptAllBtn: "Accetta tutto",
+                    acceptNecessaryBtn: "Rifiuta tutto",
+                    showPreferencesBtn: "Gestisci preferenze",
+                    footer: "<a href=\"#link\">Informativa sulla privacy</a>\n<a href=\"#link\">Termini e condizioni</a>"
+                },
+                preferencesModal: {
+                    title: "Centro preferenze per il consenso",
+                    acceptAllBtn: "Accetta tutto",
+                    acceptNecessaryBtn: "Rifiuta tutto",
+                    savePreferencesBtn: "Salva le preferenze",
+                    closeIconLabel: "Chiudi la finestra",
+                    serviceCounterLabel: "Servizi",
+                    sections: [
+                        {
+                            title: "Utilizzo dei Cookie",
+                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                        },
+                        {
+                            title: "Cookie Strettamente Necessari <span class=\"pm__badge\">Sempre Attivati</span>",
+                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                            linkedCategory: "necessary"
+                        },
+                        {
+                            title: "Cookie Analitici",
+                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                            linkedCategory: "analytics"
+                        },
+                        {
+                            title: "Ulteriori informazioni",
+                            description: "For any query in relation to my policy on cookies and your choices, please <a class=\"cc__link\" href=\"#yourdomain.com\">contact me</a>."
+                        }
+                    ]
+                }
+            }
+        }
+    }
+});
